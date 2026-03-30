cb8e9c9fc78b428d53b9d09d0c898bd8bfad6159
jefferson816 <00001137550478sp@al.educacao.sp.gov.br>
2026-03-30 12:52:31 -0300

    Add files via upload

---
+++ lanches.py
@@ -0,0 +1,8 @@
+# calculadora de lanche
+preço = int(input("digite o valor"))
+if preço <= 10:
+   print("preço justo")
+elif preço <= 20:
+   print("esta ficando caro")
+else:
+   print("muito caro, melhor levra de casa")
