f8c0e66ff9a055d698844a1a4a85c45b77aa9236
jefferson816 <00001137550478sp@al.educacao.sp.gov.br>
2026-03-31 12:57:56 -0300

    Add files via upload

---
+++ simulador.py
@@ -0,0 +1,6 @@
+simulador = input("deseja continuar jogando? (sim/não): ")
+while simulador =="não"
+if simulador =="não"
+     print("game over💀")
+if simulador =="sim"
+     print("você ainda está no jogo!!")
